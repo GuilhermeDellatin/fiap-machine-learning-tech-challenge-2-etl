6cdfb63be4c1241a88d698cef7bf9e8f01ec2a28
GuilhermeDellatin <guidellatin@gmail.com>
2025-09-16 22:04:19 -0300

    Adding lambda to trigger glue and requirements

---
+++ lambda/lambda_start_glue.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+import boto3
+
+
+def lambda_handler(event, context):
+    sfn = boto3.client('stepfunctions')
+    stepfunctions = os.environ['STEPFUNCTIONS']
+    sfn.start_execution(
+        stateMachineArn=stepfunctions,
+        input=json.dumps(event)
+    )
+    return {'status': 'Step Function started'}
